c8cdfea970692c855c7d5a81fcba02a67039431c
XTP3 <mpri349@wgu.edu>
2025-08-20 17:02:17 -0400

    Removed unused imports to example route and reverted back to commonjs in package.json

--- routes/RouteExample/RouteExample.js
+++ routes/RouteExample/RouteExample.js
@@ -1,10 +1,5 @@
-const Config = require("../../Config.json");
-const Route = require("../../Route");
-const Database = require("../../Database");
-const Utils = require("../../Utils");
-
 module.exports = async function(fastify, options) {
-    fastify.get('/pricing', {
+    fastify.get('/test', {
     }, async (request, reply) => {
         return await reply.status(200).send("Route Example is working!");
     });
